21bbd40f1ba094fcda20ed828aaefacb2d5a4e81
Michel Adrion <micheladrion@hotmail.com>
2015-06-08 20:55:32 +0300

    a

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -21,6 +21,22 @@ public class Hello extends CordovaPlugin {
         	
         	FacebookSdk.setApplicationId("732960346813030");
             FacebookSdk.sdkInitialize(context);
+			
+			String appLinkUrl, previewImageUrl;
+
+			appLinkUrl = "https://www.mydomain.com/myapplink";
+			previewImageUrl = "https://www.mydomain.com/my_invite_image.jpg";
+
+			if (AppInviteDialog.canShow()) {
+				
+				AppInviteContent content = new AppInviteContent.Builder()
+                .setApplinkUrl(appLinkUrl)
+                .setPreviewImageUrl(previewImageUrl)
+                .build();
+				
+			    AppInviteDialog.show( this.cordova.getActivity(), content);
+			    //AppInviteDialog.show( this.cordova.getActivity(), content );
+			}
 
             String name = data.getString(0);
             String message = "Hello, " + name;
